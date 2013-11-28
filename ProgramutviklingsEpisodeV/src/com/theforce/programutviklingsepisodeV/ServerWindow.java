85b792afdcac69f0848e550562a16d95d1ff933c
MaBrag <martin@bragen.net>
2013-11-28 21:52:38 +0100

    comments comments commments commments

--- ProgramutviklingsEpisodeV/src/com/theforce/programutviklingsepisodeV/ServerWindow.java
+++ ProgramutviklingsEpisodeV/src/com/theforce/programutviklingsepisodeV/ServerWindow.java
@@ -3,9 +3,19 @@ package com.theforce.programutviklingsepisodeV;
 import java.util.List;
 
 import jerklib.Session;
-
+/**
+ * Parent class.
+ * @author Hans Martin Bragen
+ * @author Jehans Jr. Storvik
+ * @author John Høegh-Omdal
+ *
+ */
 @SuppressWarnings("serial")
 public class ServerWindow extends Window {
+	/**
+	 * adds an event listener to the connect to the server
+	 * @param pSession the session of the server connection.
+	 */
 	public ServerWindow(Session pSession) {
 		super(pSession, pSession.getRequestedConnection().getHostName() + " (Connecting...)", false);
 		
@@ -15,7 +25,9 @@ public class ServerWindow extends Window {
 		// Give user some feedback
 		this.appendToChat(new ChatText().addSystemMessage("Connecting to " + pSession.getRequestedConnection().getHostName() + "..."));
 	}
-	
+	/**
+	 * Disposes itself from the channel managers list of open windows before it dies.
+	 */
 	@Override
 	protected void onClose() {
 		List<ChannelWindow> channels = Launcher.getManager().findChannelWindows(this.getSession());
